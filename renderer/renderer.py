ed13806ab1b2f326662a518730eba7e94ade3632
Ilya Sapronov <ilya@sapronov.net>
2022-02-16 02:29:54 +0300

    Finished working pre-version

--- renderer/renderer.py
+++ renderer/renderer.py
@@ -3,6 +3,7 @@ from typing import List, Text
 import fire
 import jinja2
 import openpyxl
+import pdfkit
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
@@ -17,7 +18,14 @@ def card_data_list(
 ) -> List:  # cardlist in future
     ws = openpyxl.load_workbook(file).worksheets[0].rows
     keys = [cell.value.replace(" ", "_") for cell in next(ws)]
-    return list(map(lambda row: dict(zip(keys, [cell.value for cell in row])), ws))
+    return list(
+        map(
+            lambda row: dict(
+                zip(keys, [cell.value if cell.value != "-" else "" for cell in row])
+            ),
+            ws,
+        )
+    )
 
 
 def generate_card_html(
@@ -31,23 +39,29 @@ def generate_table_html(
     cards: List, template_file: str = "templates/table_template.html"
 ) -> Text:
     template = templateEnv.get_template(template_file)
-    return template.render(cards)
+    return template.render(cards=cards)
 
 
 def get_sliced_lst(lst: List) -> List:  # For 'generate_table_html' function
     sliced_lst = []
     i = -1
     for i in range(len(lst) // 3):
-        print(lst[3 * i : 3 * i + 3])
         sliced_lst.append(lst[3 * i : 3 * i + 3])
-    print(i)
     last = lst[3 * i + 3 :]
     if last != []:
         sliced_lst.append(last)
     return sliced_lst
 
 
-# with open("my_new_file.html", "w") as fh: # output in future
-card_lst = card_data_list()
-print(card_lst)
-# fh.write(generate_table_html(map(lambda i: card_lst[3*i,3*i+3], range(len(card_lst)//3 - 1))]))
+dtlst = card_data_list()
+card_lst = list(map(generate_card_html, dtlst))
+card_lst += [""] * ((9 - len(card_lst) % 9) % 9)
+sliced_card_lst = get_sliced_lst(get_sliced_lst(card_lst))
+i = 0
+
+for card_page in sliced_card_lst:
+    with open(f"{i}out.html", "w") as fh:
+        fh.write(generate_table_html(card_page))
+    i += 1
+for j in range(i):
+    pdfkit.from_file(str(j) + "out.html", str(j) + output)
