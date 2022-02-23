65f42a1ad68c850168098e858524a06f49bb1ca6
Ilya Sapronov <ilya@sapronov.net>
2022-02-23 12:33:11 +0300

    Csv dataframe included and renderer.py refactored

--- renderer/renderer.py
+++ renderer/renderer.py
@@ -1,49 +1,46 @@
-from typing import List, Text
+from typing import Dict, List, Text
 from PyPDF2 import PdfFileMerger
 import fire
 import jinja2
 import openpyxl
 import pdfkit
+import tempfile
 import os
-
+import re
+  
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 
-cardlist, output = fire.Fire(
-    lambda cardlist, output: (cardlist, output)
-)
-
-
-def card_data_list(
-    file: str = cardlist # "dataframes/test_dataframe.xlsx",
-) -> List:
-    ws = openpyxl.load_workbook(file).worksheets[0].rows
+def read_library(path: str) -> Dict:
+    ws = openpyxl.load_workbook(path).worksheets[0].rows
     keys = [cell.value.replace(" ", "_") for cell in next(ws)]
-    return list(
+    return dict(
+        map(lambda dic: (dic['Name'], dic),
         map(
             lambda row: dict(
                 zip(keys, [cell.value if cell.value != "-" else "" for cell in row])
             ),
             ws,
         )
+        )
     )
 
+def read_deck(path) -> List:
+    ret_lst = []
+    with open(path, 'r') as deck:
+        for line in deck.readlines():
+            ret_lst.append(re.search('(\d+) (.+)$',line).groups())
+    return ret_lst
 
-def generate_card_html(
-    data_dict: dict, template_file: str = "templates/test_card_template.html"
-) -> Text:
+def render_card_html(library_dict: dict, template_file: str) -> Text:
     template = templateEnv.get_template(template_file)
-    return template.render(**data_dict)
+    return template.render(**library_dict)
 
-
-def generate_table_html(
-    cards: List, template_file: str = "templates/table_template.html"
-) -> Text:
+def render_table_html(cards_table: List, template_file: str = 'templates/table_template.html') -> Text:
     template = templateEnv.get_template(template_file)
-    return template.render(cards=cards)
+    return template.render(cards=cards_table)
 
-
-def get_sliced_lst(lst: List) -> List:  # For 'generate_table_html' function
+def get_sliced_lst(lst: List) -> List[List]:
     sliced_lst = []
     i = -1
     for i in range(len(lst) // 3):
@@ -53,33 +50,46 @@ def get_sliced_lst(lst: List) -> List:  # For 'generate_table_html' function
         sliced_lst.append(last)
     return sliced_lst
 
+def generate_tables_of_cards(library_dict: dict, deck: List, template_path: Text) -> List[List]:
+    cards_jinja_dicts = []
+    for card in deck:
+        cards_jinja_dicts += [library_dict[card[1]]]*int(card[0])
+    cards_list = list(map(lambda card: render_card_html(card, template_path), cards_jinja_dicts))
+    cards_list += [""] * ((9 - len(cards_list) % 9) % 9)
+    cards_table = get_sliced_lst(get_sliced_lst(cards_list))
+    return cards_table
 
-dtlst = card_data_list()
-card_lst = list(map(generate_card_html, dtlst))
-card_lst += [""] * ((9 - len(card_lst) % 9) % 9)
-sliced_card_lst = get_sliced_lst(get_sliced_lst(card_lst))
-pdfs = []
-for pair in enumerate(sliced_card_lst):
-    pdfkit.from_string(
-        generate_table_html(pair[1]),
-        str(pair[0]) + 'out.pdf',
-        options={
-            "--margin-bottom": "0mm",
-            "--margin-left": "0mm",
-            "--margin-right": "0mm",
-            "--margin-top": "0mm",
-        },
-    )
-    pdfs.append(str(pair[0])+ 'out.pdf')
+def render_pdf_from_html_tables(html_tables_list: List, output: str):
+    with tempfile.TemporaryDirectory() as td:
+        pdfs = []
+        for ind_and_table in enumerate(html_tables_list):
+            pdfkit.from_string(
+                ind_and_table[1],
+                os.path.join(td, f'{ind_and_table[0]}out.pdf'),
+                options={
+                    "--margin-bottom": "0mm",
+                    "--margin-left": "0mm",
+                    "--margin-right": "0mm",
+                    "--margin-top": "0mm",
+                },
+            )
+            pdfs.append(os.path.join(td, f'{ind_and_table[0]}out.pdf'))
 
-merger = PdfFileMerger()
+        merger = PdfFileMerger()
 
-for pdf in pdfs:
-    merger.append(pdf)
+        for pdf in pdfs:
+            merger.append(pdf)
 
-merger.write(output)
-merger.close()
+        merger.write(output)
+        merger.close()
 
+def render_page_cli(library_path='dataframes/test_dataframe.xlsx', deck_path='decks/test_deck.txt', template_path = "templates/test_card_template.html", output='out.pdf') -> None:
+  library_dict = read_library(library_path)
+  deck = read_deck(deck_path)
+  render_pdf_from_inputs(library_dict, deck, template_path, output)
 
-for pdf in pdfs:
-    os.remove(pdf)
+def render_pdf_from_inputs(library_dict, deck, template_path, output) -> None:
+    render_pdf_from_html_tables(list(map(render_table_html, generate_tables_of_cards(library_dict, deck, template_path))), output)
+    
+if __name__ == "__main__":
+  fire.Fire(render_page_cli)
