0ff9f4a010b9a1f0fc583165fcbf3a88c8f77ab0
marqueewinq <marqueewinq@gmail.com>
2022-03-11 11:45:30 +0300

    removed timeout

--- unit_test/test_renderer.py
+++ unit_test/test_renderer.py
@@ -381,7 +381,6 @@ def test_read_lib_happy(df_path, expected):
     assert read_library(df_path) == expected
 
 
-@pytest.mark.timeout(10)
 @pytest.mark.parametrize(
     "df_path,deck_path",
     [
